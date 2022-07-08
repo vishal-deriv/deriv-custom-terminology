fe653c9498edf6950c4e704c7a53f224dd6227b6
Adam Rrudolf <adamrbnr@gmail.com>
2022-07-08 17:43:42 +0200

    Update

---
+++ appsScript.js
@@ -0,0 +1,74 @@
+// App Script read each sheet and combine them into one file
+// map [English, Portugese, French, Spanish, Indonesian]
+// to [ en, pt, fr, es, id ]
+// https://docs.aws.amazon.com/translate/latest/dg/what-is-languages.html
+const languageCodes = {
+    English: 'en',
+    Portugese: 'pt',
+    French: 'fr',
+    Spanish: 'es',
+    Indonesian: 'id',
+} 
+
+// get first row of first sheet
+const SpreadSheet = SpreadsheetApp.getActiveSpreadsheet();
+const allSheets = SpreadSheet.getSheets();
+const sheet1 = allSheets[0];
+const languageNamesRow = sheet1.getDataRange().getValues()[0];
+const languageCodesRow = languageNamesRow.map(languageName => {
+    return languageCodes[languageName];
+});
+
+function combineSheets(sheets) {
+    const data = [];
+    sheets.forEach(sheet => {
+        const sheetData = sheet.getDataRange().getValues();
+        // delete the first row
+        sheetData.shift();
+        data.push(sheetData);
+    });
+    // flatten data
+    const flatData = data.reduce((acc, curr) => {
+        return acc.concat(curr);
+    }
+    , []);
+    return flatData;
+}
+
+function prepareTerminologyCSV() {
+    const data = combineSheets(allSheets);
+    // make languageCodesRow the first row
+    data.unshift(languageCodesRow);
+    // convert [[],[]] data to csv
+    const csv = data.join('\n');
+    return csv;
+  }
+
+
+// make POST request to https://api.github/com/repos/deriv-pocs/custom-terminology/dispatches
+// with the following -H "Authorization: token your-token-here"
+// -H "Accept: application/vnd.github+json" \ 
+// and the following -d '{"event_type": "translation_updated"}'
+function notifyGithub() {
+    const terminlogy = prepareTerminologyCSV();
+    const url = 'https://api.github.com/repos/deriv-pocs/custom-terminology/dispatches';
+    // get github token from GITHUB_TOKEN script property
+    const token = PropertiesService.getScriptProperties().getProperty('GITHUB_TOKEN');
+    const headers = {
+        'Authorization': `token ${token}`,
+        'Accept': 'application/vnd.github+json',
+        'Content-Type': 'application/json'
+    };
+    const body = {
+        event_type: 'terminology_updated',
+        client_payload: {
+            terminology: terminlogy
+        }
+    };
+    const options = {
+        method: 'POST',
+        headers: headers,
+        payload: JSON.stringify(body)
+    };
+    UrlFetchApp.fetch(url, options);
+}
