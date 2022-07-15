37479efaf11d92520adc64c75007b904d5b10a13
Adam Rrudolf <adamrbnr@gmail.com>
2022-07-15 12:23:06 +0200

    Adjust the script for not translated

--- appsScript.js
+++ appsScript.js
@@ -33,9 +33,9 @@ function combineSheets(sheets) {
                 if (cell.includes(',')) {
                     row[index] = `"${cell}"`;
                 }
-                // remove white space or tab from cell and check if it is empty and set Not translated if so
+                // remove white space or tab from cell and check if it is empty and set to first cell of the row
                 if (cell.trim() === '') {
-                    row[index] = 'Not translated';
+                    row[index] = row[0];
                 }
             }
         )});
